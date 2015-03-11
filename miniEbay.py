23499e9383c7ce7eeb0fd766396c387e4a1f0481
Tyler Larson <tylerlarson1025@hotmail.com>
2015-03-10 18:52:58 -0700

    More work on basic site layout - Added a generic search page - Added a generic browse page - Set up links between the three pages - Made nav background slightly darker

--- miniEbay.py
+++ miniEbay.py
@@ -2,13 +2,23 @@ import web
 
 render = web.template.render('templates/')
 urls = (
-  '/', 'index'
+  '/', 'index',
+  '/search', 'search',
+  '/browse', 'browse'
 )
 
 class index:
   def GET(self):
     return render.index()
 
+class search:
+	def GET(self):
+		return render.search()
+	
+class browse:
+	def GET(self):
+		return render.browse()
+	
 if __name__ == "__main__":
   app = web.application(urls, globals())
   app.run()
