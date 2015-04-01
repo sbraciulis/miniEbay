51547fc64a6eaad12eb463418358a2fc86032e4e
Tyler Larson <tylerlarson1025@hotmail.com>
2015-04-01 11:40:07 -0700

    current working version

--- miniEbay.py
+++ miniEbay.py
@@ -1,10 +1,13 @@
 import web
 
+db = web.database(dbn='mysql', user='cs351', pw='pass', db='miniEbay')
 render = web.template.render('templates/')
 urls = (
   '/', 'index',
   '/search', 'search',
-  '/browse', 'browse'
+  '/browse', 'browse',
+  '/results', 'results',
+  '/auction', 'auction'
 )
 
 class index:
@@ -12,13 +15,29 @@ class index:
     return render.index()
 
 class search:
-	def GET(self):
-		return render.search()
-	
+  def GET(self):
+    return render.search()
+
 class browse:
-	def GET(self):
-		return render.browse()
-	
+  def GET(self):
+    return render.browse()
+
+class results:
+  def POST(self):
+    query = web.data().split('=')[1]
+    auctionList = db.select('auctions')
+    results = []
+    
+    for auction in auctionList:
+      if query.lower() in auction.title.lower().replace(" ", ""):
+        results.append(auction)
+      
+    return render.results(results)
+  
+class auction:
+  def GET(self):
+    return render.auction()
+  
 if __name__ == "__main__":
   app = web.application(urls, globals())
   app.run()
